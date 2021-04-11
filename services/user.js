44eecd61cf1c147e9cd45d1917e5fb9f9b900aa1
Mayur <mayuragarwal2812@gmail.com>
2021-04-11 14:42:23 +0530

    commit

--- services/user.js
+++ services/user.js
@@ -4,21 +4,70 @@ const model = require("../models/user");
 let userService = {};
 
 userService.addtoCart = async(product) => {
+  if(validator.addtoCart(product))
   return await model.addtoCart(product);
+  else
+  {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
 userService.cartQuan = async(product) => {
+  if(validator.cartQuan(product))
   return await model.cartQuan(product);
+  else
+  {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
 userService.getfromCart = async(userid) => {
+  if(validator.getfromCart(userid))
   return await model.getfromCart(userid);
+  else
+  {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
 userService.cartDelete = async(obj) => {
+  if(validator.cartDelete(obj))
   return await model.cartDelete(obj);
+  else
+  {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
 userService.getcartCount = async(userid) => {
+  if(validator.getfromCart(userid))
   return await model.getcartCount(userid);
+  else
+  {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
 };
 userService.getcartEmpty = async(userid) => {
+  if(validator.getfromCart(userid))
   return await model.getcartEmpty(userid);
+  else
+  {
+    let err = new Error();
+    err.status = 400;
+    err.message = "Item not found,Please check the item and try again";
+    throw err;
+  }
+
 };
 module.exports = userService;
---
+++ utilities/validate.js
@@ -0,0 +1,78 @@
+const joi = require("joi");
+
+let validator = {};
+validator.addtoCart = (obj) => {
+  const schema = joi.object({
+    quantity: joi.number().min(1).max(5).required(),
+    size: joi.string().valid("XS", "S", "M", "L", "XL").required(),
+    color: joi.string().required(),
+    image: joi.string().required(),
+    _id: joi.number().min(1).required(),
+    name: joi.string().required(),
+    description: joi.string().required(),
+    rating: joi.number().min(1).max(5).required(),
+    idealFor: joi.string().valid("men", "women", "kids").required(),
+    category: joi.string().valid("Casual", "Formal", "Sports").required(),
+    price: joi.number().min(1).required(),
+    discount: joi.number().min(0).max(100).required(),
+    shippingCharges: joi.number().min(0).required(),
+    userid: joi.string().required(),
+  });
+  const { error, value } = schema.validate(obj);
+  if (error) return false;
+  else return true;
+};
+
+validator.cartQuan = (obj) => {
+  obj.quantity = +obj.quantity;
+  const schema = joi.object({
+    quantity: joi.number().min(1).max(5).required(),
+    size: joi.string().valid("XS", "S", "M", "L", "XL").required(),
+    color: joi.string().required(),
+    image: joi.string().required(),
+    _id: joi.number().min(1).required(),
+    name: joi.string().required(),
+    description: joi.string().required(),
+    rating: joi.number().min(1).max(5).required(),
+    idealFor: joi.string().valid("men", "women", "kids").required(),
+    category: joi.string().valid("Casual", "Formal", "Sports").required(),
+    price: joi.number().min(1).required(),
+    discount: joi.number().min(0).max(100).required(),
+    shippingCharges: joi.number().min(0).required(),
+    userid: joi.string().required(),
+  });
+  const { error, value } = schema.validate(obj);
+  if (error) return false;
+  else return true;
+};
+
+validator.getfromCart = (id) => {
+  const schema = joi.string().length(36).required();
+  const { error, value } = schema.validate(id);
+  if (error) return false;
+  else return true;
+};
+
+validator.cartDelete = (obj) => {
+  const schema = joi.object({
+    quantity: joi.number().min(1).max(5).required(),
+    size: joi.string().valid("XS", "S", "M", "L", "XL").required(),
+    color: joi.string().required(),
+    image: joi.string().required(),
+    _id: joi.number().min(1).required(),
+    name: joi.string().required(),
+    description: joi.string().required(),
+    rating: joi.number().min(1).max(5).required(),
+    idealFor: joi.string().valid("men", "women", "kids").required(),
+    category: joi.string().valid("Casual", "Formal", "Sports").required(),
+    price: joi.number().min(1).required(),
+    discount: joi.number().min(0).max(100).required(),
+    shippingCharges: joi.number().min(0).required(),
+    userid: joi.string().required(),
+  });
+  const { error, value } = schema.validate(obj);
+  if (error) return false;
+  else return true;
+};
+
+module.exports = validator;
